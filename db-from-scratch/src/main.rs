03433da177dc20120b3d53b7ed3a816c7fb7c546
Olivier Desenfans <desenfans.olivier@gmail.com>
2020-02-16 00:19:13 +0100

    Skeleton for meta commands and statements

--- db-from-scratch/src/main.rs
+++ db-from-scratch/src/main.rs
@@ -1,8 +1,36 @@
 use std::io::{self, BufRead, Write};
 
+enum MetaCommand {
+    EXIT,
+}
+
+enum Statement {
+    INSERT,
+    SELECT,
+}
+
 fn print_prompt() {
     print!("db > ");
     let _ = io::stdout().flush();
+}
+
+fn parse_meta_command(line: &String) -> Result<MetaCommand, String> {
+    match line.as_str() {
+        ".exit" => Ok(MetaCommand::EXIT),
+        _ => Err(format!("Unrecognized command: '{}'", line)),
+    }
+}
+
+fn prepare_statement(line: &String) -> Result<Statement, String> {
+    if line.starts_with("insert") {
+        Ok(Statement::INSERT)
+    }
+    else if line.starts_with("select") {
+        Ok(Statement::SELECT)
+    }
+    else {
+        Err(format!("Unrecognized command: '{}'", line))
+    }
 }
 
 fn main() {
@@ -10,37 +38,20 @@ fn main() {
     loop {
         print_prompt();
         let line = stdin.lock().lines().next().unwrap().unwrap();
-
-        match line.as_str() {
-            ".exit" => std::process::exit(0),
-            _ => println!("Unrecognized command: '{}'", line),
+        if line.starts_with(".") {
+            match parse_meta_command(&line) {
+                Ok(_) => std::process::exit(0),
+                Err(e) => println!("{}", e),
+            }
         }
+        else {
+            match prepare_statement(&line) {
+                Ok(Statement::INSERT) => println!("Inserting..."),
+                Ok(Statement::SELECT) => println!("Selecting stuff..."),
+                Err(e) => println!("{}", e),
+            }
+        }
+
+
     }
 }
-
-// std::string read_input()
-// {
-//     std::string line;
-//     std::getline(std::cin, line);
-//     return line;
-// }
-//
-// void print_prompt()
-// {
-//     std::cout << ("db > ");
-// }
-//
-// int main(int, char*[])
-// {
-//     while (true) {
-//         print_prompt();
-//         const auto line = read_input();
-//
-//         if (line == ".exit") {
-//             exit(EXIT_SUCCESS);
-//         }
-//         else {
-//             std::cout << "Unrecoginized command: '" << line << "'.\n";
-//         }
-//     }
-// }
